28c2489f5ddbf7c4f33359482b5742a8d2ae7a15
masrafi-000 <smmasrafi000@gmail.com>
2025-03-24 19:47:56 +0600

    BackEnd Finished

--- auth-jwt/server/controllers/authController.js
+++ auth-jwt/server/controllers/authController.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import userModel from "../models/userModel.js";
 
 // Register Controller
 export const register = async (req, res) => {
@@ -119,3 +119,207 @@ export const logout = async (req, res) => {
         return res.status(500).json({ success: false, message: "Server error" });
     }
 };
+
+// Send Verification otp to the user email
+export const sendVerifyOtp = async (req, res) => {
+    try {
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if (user.isAccountVerified) {
+            return res.json({
+                success: false,
+                message: "Account Already verified"
+            })
+        }
+
+       const otp = String(Math.floor(100000 + Math.random() * 900000))
+
+       user.verifyOtp = otp;
+       user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+
+       await user.save();
+
+       const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Account Verification OTP",
+            text: `Your OTP is ${otp}. Verify your account using this OTP. `,
+       }
+
+       await transporter.sendMail(mailOption);
+
+       res.json({
+        success: true,
+        message: 'Verification OTP Sent on Email'
+       })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message})
+    }
+}
+
+// verify email by otp
+export const verifyEmail = async (req, res) => {
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp ) {
+        return res.json({
+            success: false,
+            message: "Missing Details"
+        });
+    }
+    
+    try {
+        
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        if (user.verifyOtp === '' || user.verifyOtp != otp) {
+            return res.json({
+                success: false,
+                message: "Invalid OTP"
+            });
+        }
+
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.json({
+                success: false,
+                message: "OTP Expired"
+            });
+        }
+
+        user.isAccountVerified = true;
+
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+
+        return res.json({
+            success: true,
+            message: "Email verified successfully"
+        });
+
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        });
+    }
+
+}
+
+// Check if user is authenticated
+export const isAuthenticated = async (req, res) => {
+    
+    try {
+        return res.json({success: true});
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// Send Password Reset OTP
+export const sendResetOtp = async (req, res) => {
+    const {email} = req.body;
+
+    if(!email) {
+        return res.json({ success: false, message: "Email is requied"});
+    }
+
+    try {
+
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.json({ success: false, message: "User not found"
+            })
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000))
+
+       user.resetOtp = otp;
+       user.resetOtpExpireAt = Date.now() + 15 * 60 *  1000;
+
+       await user.save();
+
+       const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Password Reset OTP",
+            text: `Your OTP for resetting your password is ${otp}. Use this OTP to procced with resetting your password `,
+       }
+
+       await transporter.sendMail(mailOption);
+
+       return res.json({success: true, message: "OTP sent to your email"})
+        
+    } catch (error) {
+        return res.json({message: false, message: error.message});
+    }
+
+}
+
+// Reset User Password 
+export const resetPassword = async (req, res) => {
+    const {email, otp, newPassword} = req.body;
+
+    if (!email || !otp || !newPassword) {
+        return res.json({
+            success: false,
+            message: "Email, OTP and new passowrd are required"
+        });
+    }
+
+    try {
+
+        const user = await userModel.findOne({email})
+        if(!user) {
+            return res.json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        if (user.resetOtp === '' || user.resetOtp != otp) {
+            return res.json({
+                success: false,
+                message: "Invalid OTP"
+            });
+        }
+
+        if (user.resetOtpExpireAt < Date.now()) {
+            return res.json({
+                success: false,
+                message: "OTP Expired"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        user.password = hashedPassword;
+
+        user.resetOtp = '';
+        user.resetOtpExpireAt = 0;
+
+        await user.save();
+
+        return res.json({
+            success: true,
+            message: "Password has been reset successflly"
+        });
+        
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
--- auth-jwt/server/server.js
+++ auth-jwt/server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoute.js"
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 
@@ -18,8 +19,10 @@ app.use(cors({credentials: true}));
 // API Endpoints
 app.get('/', (req,res) => {
    res.status(200).json({ message: 'API Working'})
-})
-app.use('/api/auth', authRouter)
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`)
 );
